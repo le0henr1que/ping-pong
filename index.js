159f25378b45d05644b2c1702fdc556780f7a414
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2025-04-19 15:03:31 -0300

    Initial files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3333;
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
