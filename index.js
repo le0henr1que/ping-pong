38ee896a4ce09ac3b5d912b0ba43bbdada9cec0c
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2025-04-19 17:29:06 -0300

    add health check

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
